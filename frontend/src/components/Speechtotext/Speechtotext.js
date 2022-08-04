3bee1af191d51575b36e8acfdd88abbf6131eef7
Ak-6555 <akashsust55@gmail.com>
2022-08-04 14:12:49 +0600

    added music section

--- frontend/src/components/Speechtotext/Speechtotext.js
+++ frontend/src/components/Speechtotext/Speechtotext.js
@@ -8,13 +8,13 @@ const Pink = pink[500];
 
 const Speechtotext = () => {
     return (
-        <div>
+        <div className="mb-5">
             
             <Fab color={Pink} aria-label="SpeechIcon">
                 <img className="speechIcon" src={mic} alt="" />
            
             </Fab>
-            <h4 className="my-5 text-light">Please Command your buddy</h4>
+            <h5 className="my-4 text-light">Please Command your buddy</h5>
         </div>
     );
 };
